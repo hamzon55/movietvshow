b052fd4d3c4da05bc0291b38f7d23964bd355cef
hamzon55 <hamzon.jb@gmail.com>
2017-01-25 16:01:57 +0100

    create movie class in models

---
+++ app/src/main/java/com/lasalle/movieserie/models/Movie.java
@@ -0,0 +1,48 @@
+package com.lasalle.movieserie.models;
+
+/**
+ * Created by hamza on 25/01/2017.
+ */
+
+public class Movie {
+
+    private String name;
+    private String year;
+    private  String director ;
+
+    public Movie(String name, String year, String director) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+
+}
+
+
